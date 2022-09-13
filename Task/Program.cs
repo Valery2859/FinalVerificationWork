e2752418848f811d8817d12c6dc5ab0241b23ace
Valery <dobrinyvr@gmail.com>
2022-09-13 21:08:56 +0300

    Создание Папки и программы

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,19 +1,2 @@
-﻿/* Написать программу, которая из имеющегося массива строк формирует новый массив
-из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив 
-можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении 
-не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
-
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.Clear();
-        string[] stringArray = new string[]{"Sun", "Monday", "Tuer",
-      "Wednesday", "Th", "Fri", "Saturday"};
-        string[] result1 = new string[]{};
-        string result;
-        int b = 0;
-        result1 = new string[7];     
-
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
